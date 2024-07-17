a680a306f0b629bce697b16d3fb4bf71380ad274
SakshamSrivasta <149704928+SakshamSrivasta@users.noreply.github.com>
2024-07-17 21:49:57 +0530

    Time: 12 ms (83.73%), Space: 11.5 MB (76.4%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.py
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -4,4 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        score=0
+        for i in range(len(s)-1):
+            score+=abs(ord(s[i])-ord(s[i+1]))
+        return score
         
